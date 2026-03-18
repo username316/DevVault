7b23cf22e9c318bdb26ce48e51d8392bd61cf5b8
jtan <46615853+username316@users.noreply.github.com>
2026-03-18 13:50:30 -0400

    basic xml layout for MainActivity
    added a basic design plan for the homepage activity_main.xml and created resource_card_layout.xml for the homepage's RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
