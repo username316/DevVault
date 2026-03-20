901536cbed472a64aafdf7ae8a71936152e8b130
jtan <46615853+username316@users.noreply.github.com>
2026-03-20 16:41:40 -0400

    homepage resource list
    added classes ResourceListAdapter, ResourceDataSet for MainActivity's resource card list RecyclerView
    updated resource_card_layout's xml design
    added an add resource button to activity_main.xml (functionality to be implemented)

--- app/src/main/java/com/example/devvault/MainActivity.java
+++ app/src/main/java/com/example/devvault/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.devvault;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<ResourceDataSet> resourceDataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //TODO: fill resourceDataList
+
+        RecyclerView resourceList = findViewById(R.id.resource_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        resourceList.setLayoutManager(linearLayoutManager);
+        //ResourceListAdapter resourceListAdapter = new ResourceListAdapter(resourceDataList);
+        //resourceList.setAdapter(resourceListAdapter);
+
+        /**Button addResButton = findViewById(R.id.add_resource_button);
+        addResButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });**/
     }
 }
---
+++ app/src/main/java/com/example/devvault/ResourceDataSet.java
@@ -0,0 +1,43 @@
+package com.example.devvault;
+
+public class ResourceDataSet {
+    private String title;
+    private String type;
+    private String textContent = "";
+
+    //TODO: video
+
+    public ResourceDataSet(String title, String type) {
+        this.title = title;
+        this.type = type;
+    }
+    public ResourceDataSet(String title, String type, String textContent) {
+        this.title = title;
+        this.type = type;
+        this.textContent = textContent;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setTextContent(String textContent) {
+        this.textContent = textContent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+}
